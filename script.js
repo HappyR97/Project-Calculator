4a90891bd2c733910375cc374ba467308b473d1f
Rida Berkaoui <ridaberkaoui1997@gmail.com>
2023-01-12 10:27:01 +0100

    Fix calculator to display one operand at a time as instructed Write functions and event listeners for all operators

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-let firstNumberText = document.querySelector(".first-number");
-let operatorText = document.querySelector(".operator");
-let secondNumberText = document.querySelector(".second-number");
-let btnNumbers = document.querySelectorAll(".btn--number");
-let btnOperators = document.querySelectorAll(".btn--operator");
+const screenText = document.querySelector(".screen-text");
+const btnNumbers = document.querySelectorAll(".btn--number");
+const btnOperators = document.querySelectorAll(".btn--operator");
+const btnOperate = document.querySelector(".btn--operate");
 
-let firstNumber = NaN;
+let numbers = [];
+let total;
+let operator;
+let textCue = false;
 
 function add(a, b) {
-  return a + b;
+  return Number(a) + Number(b);
 }
 function subtract(a, b) {
-  return a - b;
+  return Number(a) - Number(b);
 }
 function multiply(a, b) {
-  return a * b;
+  return Number(a) * Number(b);
 }
 function divide(a, b) {
-  return a / b;
+  return Number(a) / Number(b);
 }
 function remainder(a, b) {
-  return a % b;
+  return Number(a) % Number(b);
 }
 
 function operate(a, b, operator) {
@@ -30,35 +32,78 @@ function operate(a, b, operator) {
   if (operator === "%") return remainder(a, b);
 }
 
-function addContent(span, content) {
-  span.textContent += content.textContent;
+function operateElements(arr, operator) {
+  return operate(arr[0], arr[1], operator);
 }
+
+// function addContent(span, content) {
+//   span.textContent += content.textContent;
+// }
 
 btnNumbers.forEach(function (btnNumber) {
   btnNumber.addEventListener("click", function () {
-    if (operatorText.textContent === "") {
-      addContent(firstNumberText, btnNumber);
+    // Change text content
+    if (!textCue) {
+      screenText.textContent += btnNumber.textContent;
+    } else {
+      screenText.textContent = "";
+      screenText.textContent += btnNumber.textContent;
+      textCue = false;
     }
-    if (operatorText.textContent !== "") {
-      addContent(secondNumberText, btnNumber);
-    }
-    // firstNumber = Number(firstNumberText.textContent);
   });
 });
 
 btnOperators.forEach(function (btnOperator) {
   btnOperator.addEventListener("click", function () {
-    if (firstNumberText.text !== "") {
-      operatorText.textContent = btnOperator.textContent;
+    console.log(operator);
+    if (textCue) textCue = false;
+    if (!textCue) textCue = true;
+    if (numbers.length < 2) numbers.push(screenText.textContent);
+    if (numbers.length >= 2 && operator) {
+      total = operateElements(numbers, operator);
+      screenText.textContent = operateElements(numbers, operator);
+      numbers = [total];
     }
+    operator = btnOperator.id;
   });
 });
 
-// btnOperators.forEach(function (btnOperator) {
-//   let buttonText = btnOperator.textContent;
-//   btnOperator.addEventListener("click", function () {
-//     if (screenText.textContent.indexOf(buttonText) === -1) {
-//       addContent(btnOperator);
+btnOperate.addEventListener("click", function () {});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// btnNumbers.forEach(function (btnNumber) {
+//   btnNumber.addEventListener("click", function () {
+//     if (totalText.textContent !== "") {
+//       totalText.textContent = "";
+//     }
+//     if (operatorText.textContent === "") {
+//       addContent(firstNumberText, btnNumber);
+//     }
+//     if (operatorText.textContent !== "") {
+//       addContent(secondNumberText, btnNumber);
 //     }
 //   });
+// });
+
+// btnOperators.forEach(function (btnOperator) {
+//   btnOperator.addEventListener("click", function () {
+//     firstNumber = Number(firstNumberText.textContent);
+//     if (firstNumberText.text !== "") {
+//       operatorText.textContent = btnOperator.textContent;
+//     }
+//   });
+// });
+
+// btnOperate.addEventListener("click", function () {
+//   secondNumber = Number(secondNumberText.textContent);
+//   if (operatorText.textContent === "+") {
+//     total = operate(firstNumber, secondNumber, "+");
+//   }
+//   totalText.textContent = total;
+//   operatorText.textContent = "";
+//   firstNumberText.textContent = "";
+//   secondNumberText.textContent = "";
 // });
